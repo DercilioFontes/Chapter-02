66fbc468048b5f9fdbe2be03a8e18f3d90ee64aa
Dercilio Fontes <dercilioafontes@gmail.com>
2020-06-26 01:04:43 -0400

    Added up to Adding Dynamic Output

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
-        public string Index()
+        public ViewResult Index()
         {
-            return "Hello World";
+            return View();
         }
     }
 }
